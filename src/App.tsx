4a34e7318891761c15c7277d6442e38c168ca398
Amitesh Anand <aamitesh7@gmail.com>
2021-06-10 07:09:09 +0530

    add todoList component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Form from "./form"
+import TodoList from "./todoList"
 function App() {
+  const todos=[{id:"t1", todoTitle:"Start working"}];
+
   return (
     <div className="App">
       <Header />
       <Form/>
+      <TodoList items={todos}/>
       <Footer />
     </div>
   );
